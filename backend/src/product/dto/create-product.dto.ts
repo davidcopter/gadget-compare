f313758885e384c354b2c7bc15cee58dd1ab700c
Phanuwat Phalad <phanuwat.davidcopter@gmail.com>
2021-05-01 15:54:13 +0700

    feat: add product create read and search

--- backend/src/product/dto/create-product.dto.ts
+++ backend/src/product/dto/create-product.dto.ts
@@ -1,1 +1,18 @@
-export class CreateProductDto {}
+import { IsNumber, IsString } from 'class-validator';
+
+export class CreateProductDto {
+  @IsString()
+  name: string;
+  @IsString()
+  description: string;
+  @IsString()
+  department: string;
+  @IsString()
+  price: string;
+  @IsString()
+  material: string;
+  @IsString()
+  dimension: string;
+  @IsNumber()
+  weight: number;
+}
--- backend/src/product/product.service.ts
+++ backend/src/product/product.service.ts
@@ -1,26 +1,37 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Like, Repository } from 'typeorm';
 import { CreateProductDto } from './dto/create-product.dto';
-import { UpdateProductDto } from './dto/update-product.dto';
+// import { UpdateProductDto } from './dto/update-product.dto';
+import { Product } from './entities/product.entity';
 
 @Injectable()
 export class ProductService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+  constructor(
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>,
+  ) {}
+
+  async create(createProductDto: CreateProductDto): Promise<Product> {
+    return await this.productRepository.create(createProductDto);
   }
 
-  findAll() {
-    return `This action returns all product`;
+  async search(word: string): Promise<Product[]> {
+    return await this.productRepository.find({
+      where: { name: Like(`%${word}%`) },
+      take: word == null ? 25 : 10,
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne(id: string): Promise<Product> {
+    return await this.productRepository.findOne(id);
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
-  }
+  // update(id: number, updateProductDto: UpdateProductDto) {
+  //   return `This action updates a #${id} product`;
+  // }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
-  }
+  // remove(id: number) {
+  //   return `This action removes a #${id} product`;
+  // }
 }
