f098eea3468259e4b22876f4ed965171304bbcc3
Phanuwat Phalad <phanuwat.davidcopter@gmail.com>
2021-05-02 02:17:07 +0700

    fix: docker port and bug

--- backend/src/product/dto/update-product.dto.ts
+++ backend/src/product/dto/update-product.dto.ts
@@ -1,6 +1,6 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString } from 'class-validator';
+import { IsNumber, IsString, IsUrl } from 'class-validator';
 import { CreateProductDto } from './create-product.dto';
 
 export class UpdateProductDto extends PartialType(CreateProductDto) {
